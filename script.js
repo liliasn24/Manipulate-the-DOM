fafaaecd212178c3f2c7e616fced31fb3561ecf2
= <liliasalas@lilias-imac.myfiosgateway.com>
2021-05-14 22:29:14 -0400

    HW done

--- script.js
+++ script.js
@@ -10,7 +10,12 @@ let quotes = [
 document.addEventListener("DOMContentLoaded", function(event) {
   // Random quote of the day generator
   const randomQuote = function() {
-    document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
+    const title = document.querySelector('#quote-title')
+      console.log(title);
+      title.addEventListener('click', (evt) => {
+        document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]}"`;
+          ;
+      })
   };
   randomQuote();
 
@@ -42,7 +47,7 @@ deleteLi();
   // Part 4
 const changeFont = function() {
   const specialTitle = document.querySelectorAll('.special-title');
-  console.log(specialTitle);
+  // console.log(specialTitle);
   for(let i = 0; i <specialTitle.length; i++){
     specialTitle[i].style.fontSize = '2rem';
   }
@@ -50,10 +55,9 @@ const changeFont = function() {
 changeFont();
 
   // Part 5
-
 const deleteRace = function() {
   const races = document.getElementById('past-races');
-  console.log(races);
+  // console.log(races);
   const removeChicago = races.children[3];
   removeChicago.remove();
 }
@@ -61,17 +65,50 @@ deleteRace();
 
 
   // Part 6
+const addCity = function() {
+  const ul = document.getElementById('past-races');
+  const addLi = document.createElement('li');
+  addLi.innerHTML = 'Prage';
+  ul.appendChild(addLi);
+}
+addCity();
 
 
   // Part 7
+const addBlog = function () {
+  const newBlog = document.getElementById('dom-adventures');
+
+  const addLiCity = document.createElement('div');
+  addLiCity.className = 'blog-post purple';
+
+  const h1 = document.createElement('h1');
+  h1.innerHTML = 'Prague';
+  console.log(h1);
+  addLiCity.appendChild(h1);
+
+  const p = document.createElement('p');
+  p.innerHTML = 'It would be a cool movie';
+  console.log(p);
+  addLiCity.appendChild(p);
+
+  console.log(addLiCity);
+
+  newBlog.appendChild(addLiCity);
+
+  // instead of writting all this code, how would the clone method work here?
+}
+
+addBlog();
 
 
   // Part 8
 
+// For part 8, see lines 10 - 20.
 
   // Part 9
-
-
-
+const addE = function () {
+  const addEvent = document.querySelector('.blog-post purple');
+  console.log(addEvent);
+}
 
 });
